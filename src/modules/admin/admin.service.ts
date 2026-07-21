f55631439dc029a48f970d7892f76b3b411405ed
arian2004 <arian2004feni@gmail.com>
2026-07-21 16:33:29 +0600

    unique constraint added on category name

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -4,19 +4,6 @@ import { ICreateCategory } from "./admin.interface";
 const createCategoryInToDB = async (payload: ICreateCategory) => {
   const { name, description } = payload;
 
-  const isCategoryExists = await prisma.category.findFirst({
-    where: {
-      name: {
-        equals: name,
-        mode: "insensitive",
-      },
-    },
-  });
-
-  if (isCategoryExists) {
-    throw new Error("Category Already Exists!");
-  }
-
   const category = await prisma.category.create({
     data: {
       name,
